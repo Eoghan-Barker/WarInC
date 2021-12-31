6d682b49cdc6e1b8a123a439b5f38b84ba97f590
Eoghan-Barker <g00397072@gmit.ie>
2021-12-31 12:01:16 +0000

    Deck and Shuffle
    Deck array of 52 initialised and shuffle function

--- WarProceduralProject/Source.c
+++ WarProceduralProject/Source.c
@@ -1,80 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+void shuffle(int *shuffledDeck);
 
 
 void main() {
 	//Variables and Arrays
-	int deckOfCards[4][13];
-/*	int player1Hand[13];
-	int player2Hand[13];
-	int player3Hand[13];
-	int player4Hand[13];*/
+	int deckOfCards[52];
 
 	int playersCardChoice[4];
 
-	int i, j, k, roundWinner;
+	int i;
 
-	const int NUM_PLAYERS = 4;
+	
 
 	//populate deck of cards array
-	//Ace = index0 value14, J,Q,K =11,12,13
-	//Suits - 0 = Hearts, 1 = Diamonds, 2 = Clubs, 3 = Spades
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 13; j++) {
-			if (j == 0) {
-				deckOfCards[i][j] = 14;
-			}
-			else {
-				deckOfCards[i][j] = j + 1;
-			}	
+	for (i = 0; i < 52; i++) {
+		//check for ace(worth 14) else assign value 2-13
+		//also need to check for kings so value is not 0
+		if (i == 0 || i == 13 || i == 26 || i == 39){
+			deckOfCards[i] = 14;
+		}
+		else if (i == 12 || i == 25 || i == 38 || i == 51) {
+			deckOfCards[i] = 13;
+		}
+		else {
+			deckOfCards[i] = (i+1) % 13;
 		}
 	}
+
 
 	//Shuffle deck
+	shuffle(deckOfCards);
 
-	//Deal cards to p1, p2, p3, p4
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 13; j++) {
-		    /*if (i == 0) {
-				player1Hand[j] = deckOfCards[j][i];
-			}
-			else if (i == 1){
-				player2Hand[j] = deckOfCards[j][i];
-			}
-			else if (i == 2) {
-				player3Hand[j] = deckOfCards[j][i];
-			}
-			else if (i == 3) {
-				player4Hand[j] = deckOfCards[j][i];
-			}*/
+	/*printf("\n");
+	for (int i = 0; i < 52; i++) {
+		printf("%d  ", deckOfCards[i]);
+	}*/
 
-		}
-	}
-
-	//play 13 rounds
-	for (i = 0; i < 13; i++) {
-		//print each players deck and ask them to choose a card to play
-		for (j = 0; j < NUM_PLAYERS; j++) {
-
-			printf("Round %d: \n", i+1);
-			printf("Player %d deck: ", j+1);
-			for (k = 0; k < 13; k++) {
-				printf("%d:%d\t", k, deckOfCards[j][k]);
-			}
-			do {
-				printf("Choose a card to play(0-12): ");
-				scanf("%d", &playersCardChoice[j]);
-				if (deckOfCards[j][playersCardChoice[j]] == -1) {
-					printf("Card already played, choose another");
-				}
-			} while (deckOfCards[j][playersCardChoice[j]] == -1);
-			
-		}
-
-		//calculate winner and add points to total
-		if(deckOfCards[0][playersCardChoice[0]])
-
-	}
+	
 	       
+}// end of main
 
+void shuffle(int *shuffledDeck) {
+	int i, j, k;
+	srand ( time(NULL) );
 
+	//move through each array index and swap the value of that index with the value of a random index
+	for (i = 0; i < 52 - 1; i++)
+	{
+		//create random number between 0-51
+		j = rand() % 52;
+		printf("%d ", j);
+		//store value from random index
+		k = shuffledDeck[j];
+		//swap random index value with current index value
+		shuffledDeck[j] = shuffledDeck[i];
+		shuffledDeck[i] = k;
+	}
 }
